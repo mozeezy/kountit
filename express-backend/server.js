fcc1f605bdd69bcb8c0fcf57a917ca6a5a744ced
Mohamed <justmoe23@gmail.com>
2022-11-30 18:27:06 -0500

    initialize an express server

---
+++ express-backend/server.js
@@ -0,0 +1,19 @@
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv').config()
+
+// Initialize an express app
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.get("/", (req, res) => {
+  res.send("Hello I'm Live!")
+})
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`)
+})
