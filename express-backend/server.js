8e7daf2b16f5f16ec0c4e67ecf317cb47663a516
Mohamed <justmoe23@gmail.com>
2022-12-13 19:53:28 -0500

    connect to mongodb using mongoose

--- express-backend/server.js
+++ express-backend/server.js
@@ -1,19 +1,27 @@
 // Import dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 
 // Initialize an express app
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-
+// Test the home route
 app.get("/", (req, res) => {
-  res.send("Hello I'm Live from my app!")
-})
+  res.send("Hello I'm live from the home route!");
+});
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`)
-})
+// Connect to MongoDB Database
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
