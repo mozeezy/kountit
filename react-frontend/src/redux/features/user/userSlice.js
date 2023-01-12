d4ded871c97216a38768d787dcbe3bb6151af59b
Mohamed <justmoe23@gmail.com>
2023-01-12 17:54:41 -0500

    setup redux toolkit by creating a store and a reducer

---
+++ react-frontend/src/redux/features/user/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
