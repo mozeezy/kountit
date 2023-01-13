8d66023536e1422654c9fd469f4e5ab8cce2c532
Mohamed <justmoe23@gmail.com>
2023-01-13 16:50:19 -0500

    create custom reusable card component

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Register from "./components/Forms/Register"
+import Login from "./components/Forms/Login"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ react-frontend/src/components/Forms/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const Login = () => {
+  return (
+    <Card>
+      <h1>Login</h1>
+    </Card>
+  );
+};
+
+export default Login;
