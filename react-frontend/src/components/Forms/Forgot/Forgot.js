203b8d8807cdfe19e72a3da18b6f10559abf423f
Mohamed <justmoe23@gmail.com>
2023-01-13 20:58:50 -0500

    create the forgot password form component

--- react-frontend/src/components/Forms/Forgot/Forgot.js
+++ react-frontend/src/components/Forms/Forgot/Forgot.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Card from "../../Card/Card";
+import classes from "../Login/login.module.css";
+import { CiHome } from "react-icons/ci";
+import { MdPassword } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Forgot = () => {
   return (
-    <div>
-      <h2>Forgot Password</h2>
+    <div className={`container ${classes.login}`}>
+      <Card>
+        <div className={classes.form}>
+          <div className={classes.home}>
+            <Link to="/">
+              <CiHome size={25}></CiHome>
+            </Link>
+          </div>
+          <div className={classes.login_icon}>
+            <MdPassword size={40}></MdPassword>
+          </div>
+          <br />
+          <h2>Forgot Password Form</h2>
+          <br />
+          <form>
+            <input
+              type="email"
+              placeholder="📧 E-mail address"
+              required
+              name="email"
+            />
+            <button type="submit" className={classes.btn}>
+              Submit
+            </button>
+          </form>
+          <br />
+          <span className="action-call">
+            <p>Already a member? &nbsp;</p>
+            <Link to="/login">Click here to login</Link>
+          </span>
+        </div>
+      </Card>
     </div>
   );
 };
