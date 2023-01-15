3f212e442c56a816b27f9855cf415ee31f53f079
Mohamed <justmoe23@gmail.com>
2023-01-15 17:22:01 -0500

    finish setting userSlice and create a preloading screen

--- react-frontend/src/redux/features/user/userSlice.js
+++ react-frontend/src/redux/features/user/userSlice.js
@@ -1,15 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const name = JSON.parse(localStorage.getItem("name"));
+
 const initialState = {
   isLoggedIn: false,
+  name: name ? name : "",
+  userData: {
+    name: "",
+    email: "",
+  },
+  userID: "",
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
+    },
+    SET_USERNAME(state, action) {
+      localStorage.setItem("name", JSON.stringify(action.payload));
+      state.name = action.payload;
+    },
+    SAVE_USERDATA(state, action) {
+      const userProfile = action.payload;
+      state.userData.name = userProfile.name;
+      state.userData.email = userProfile.email;
+    },
+  },
 });
 
-export const {} = userSlice.actions;
+export const { SET_LOGIN, SET_USERNAME, SAVE_USERDATA } = userSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.user.isLoggedIn;
+export const selectName = (state) => state.user.name;
+export const selectUserData = (state) => state.user.userData;
+export const selectUserId = (state) => state.user.userID;
 
 export default userSlice.reducer;
