095b3fe5e6a60f23b7c4da075b4d84f1fa6f551d
Mohamed <justmoe23@gmail.com>
2023-01-16 01:23:29 -0500

    redo the homepage component

--- react-frontend/src/components/Home/Home.js
+++ react-frontend/src/components/Home/Home.js
@@ -1,62 +1,76 @@
-import React from "react";
-import "./home.css";
-import { TbLetterK } from "react-icons/tb";
 import { Link } from "react-router-dom";
+import "./home.css";
 import headerIMG from "../../assets/inventory_solution.jpeg";
 
-function Home() {
+const Home = () => {
   return (
-    <div id="home">
-      <nav>
-        <Link to="/">
-          <div className="logo">
-            <TbLetterK size={40} />
+    <div>
+      <nav
+        className="navbar navbar-expand-lg p-4 m-3 fixed-top bg-light shadow"
+        style={{ borderRadius: "80px" }}
+      >
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <h4 className="fw-bold">Kountit.</h4>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor03"
+            aria-controls="navbarColor03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor03">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="btn btn-outline-info me-3" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="btn btn-info" to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
           </div>
-        </Link>
-
-        <ul className="container--links">
-          <li className="register">
-            <Link to="/register">Register</Link>
-          </li>
-            <button className="btn login-btn">
-              <Link to="/login">Login</Link>
-            </button>
-        </ul>
+        </div>
       </nav>
-
-      <section className="container landing">
-        <div className="landing-container">
-          <article className="landing-text">
-            <h2>
-              Welcome to Kountit - an Inventory and Stock Management Solution
-            </h2>
-            <br />
-            <p>
-              Kountit is the go-to warehouse inventory management solution. By
-              reducing the steps in managing inventory, Kountit allows
-              businesses of any size to keep accurate track of inventory and
-              stock and scale your business. Improve efficiency and save money
-              with real-time inventory updates.{" "}
-            </p>
-            <br />
-            <button className="btn">
-              <Link to="/dashboard" className="started-txt">
+      <div id="main-section" className="section-padding">
+        <div className="container">
+          <div className="row align-items-center gy-4">
+            <div className="col-lg-6">
+              <img src={headerIMG} className="img-fluid" alt="kountit" />
+            </div>
+            <div className="col-lg-6">
+              <h1 className="fw-bold display-6">
+                Welcome to Kountit
+                <br />
+                <span className="text-info">
+                  {" "}
+                  Inventory and Stock Management Solution
+                </span>
+              </h1>
+              <p>
+                Kountit is the go-to warehouse inventory management solution. By
+                reducing the steps in managing inventory, Kountit allows
+                businesses of any size to keep accurate track of inventory and
+                stock and scale your business. Improve efficiency and save money
+                with real-time inventory updates.{" "}
+              </p>
+              <Link to="/dashboard" className="btn btn-outline-info">
                 Get Started
               </Link>
-            </button>
-          </article>
-
-          <article className="landing-image">
-            <img
-              src={headerIMG}
-              alt="Kountit"
-              className="container landing-image"
-            />
-          </article>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
