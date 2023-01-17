59de4c262d2003e373d77c2430a1f3331d92b655
Mohamed <justmoe23@gmail.com>
2023-01-17 15:45:32 -0500

    create dashboard navbar

--- react-frontend/src/components/Sidebar/Sidebar.js
+++ react-frontend/src/components/Sidebar/Sidebar.js
@@ -4,20 +4,12 @@ import "./sidebar.css";
 import { SideBarItem } from "./SideBarItem";
 import { Link } from "react-router-dom";
 import { CgWindows } from "react-icons/cg";
+import NavDash from "../NavDash/NavDash";
 
 const Sidebar = ({ children }) => {
   return (
     <div className="sidebar">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-3 sm-1 mt-3 p-2">
-            <Link to="/">
-              <h3>Kountit</h3>
-            </Link>
-          </div>
-        </div>
-        <div className="col-lg-9 md-6 sm-11">{children}</div>
-      </div>
+      <NavDash />
     </div>
   );
 };
