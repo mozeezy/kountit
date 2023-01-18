878bdfc71a31d1ebb5bef55eb60c721209939722
Mohamed <justmoe23@gmail.com>
2023-01-17 21:45:36 -0500

    finish styling the main dashboard

--- react-frontend/src/components/Sidebar/Sidebar.js
+++ react-frontend/src/components/Sidebar/Sidebar.js
@@ -4,12 +4,64 @@ import "./sidebar.css";
 import { SideBarItem } from "./SideBarItem";
 import { Link } from "react-router-dom";
 import { CgWindows } from "react-icons/cg";
-import NavDash from "../NavDash/NavDash";
+import { IoMdLogOut } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="sidebar">
-      <NavDash />
+      <div
+        className="sidebar_container"
+        style={{ display: isOpen ? "block" : "none" }}
+      >
+        <Card className="sidebar_card">
+          <div className="sidebar_title">
+            <h2>Kountit</h2>
+          </div>
+          <div className="sidebar_user">
+            <CgWindows size={30} className="sidebar_user_icon" />
+            <h3 className="sidebar_user_text">Mohamed Mohamed</h3>
+            <p>ID: 12345</p>
+          </div>
+          <ul className="navbar-nav ms-auto pb-2">
+            {SideBarItem.map((value, index) => (
+              <Link to={value.path} style={{ textDecoration: "none" }}>
+                <li
+                  className="btn btn-lg btn-primary mb-4"
+                  key={index}
+                  id={window.location.pathname === value.path ? "active" : ""}
+                >
+                  <div className="sidebar_items">
+                    {value.icon}
+                    <div className="sidebar_text">{value.title}</div>
+                  </div>
+                </li>
+              </Link>
+            ))}
+          </ul>
+          <div className="logout_icon">
+            <Link to="/logout" className="btn btn-lg btn-primary">
+              <IoMdLogOut />
+            </Link>
+          </div>
+        </Card>
+      </div>
+      <div id="hamburger_icon">
+        <button
+          className="btn btn-lg btn-primary"
+          type="button"
+          onClick={toggle}
+        >
+          <RxHamburgerMenu size={20} />
+        </button>
+      </div>
+      <main>{children}</main>
     </div>
   );
 };
--- react-frontend/src/components/Sidebar/SidebarItem.js
+++ react-frontend/src/components/Sidebar/SidebarItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import { RxDashboard } from "react-icons/rx";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
-import { IoMdLogOut } from "react-icons/io";
 
 export const SideBarItem = [
   {
@@ -19,10 +18,5 @@ export const SideBarItem = [
     title: "Profile",
     icon: <CgProfile size={20} />,
     path: "/profile",
-  },
-  {
-    title: "Logout",
-    icon: <IoMdLogOut size={20} />,
-    path: "/logout",
   },
 ];
