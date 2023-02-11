5444a600d0b830924c00bf62e495c78882214467
Mohamed <justmoe23@gmail.com>
2023-02-10 20:35:55 -0500

    more tests

--- express-backend/__tests__/products.test.js
+++ express-backend/__tests__/products.test.js
@@ -2,8 +2,17 @@ const supertest = require("supertest");
 const app = require("../app");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const authorize = require("../middleware/authorize");
+
+const userId = new mongoose.Types.ObjectId().toString();
 
 require("dotenv").config();
+
+const mockUser = {
+  _id: userId,
+  email: "example@example.com",
+  name: "Example 1",
+};
 
 beforeAll(async () => {
   const mongoServer = await MongoMemoryServer.create();
@@ -16,7 +25,12 @@ afterAll(async () => {
 });
 
 describe("Testing Product Routes", () => {
-  it("should return a 400 status code if product doesn't exist", () => {
+  it("should return a 401 status code if a logged out user tried to create a product", async () => {
+    const { statusCode } = await supertest(app).post("/api/products");
+    expect(statusCode).toBe(401);
+  });
+
+  it("should return a 200 status code if the user is logged and tried to create a product", async () => {
     expect(true).toBe(true);
   });
 });
