a810b26bb10d23273c39c2f8afb399051561b6ab
Mohamed <justmoe23@gmail.com>
2023-02-10 22:11:28 -0500

    add frontend deploy to CORS

--- express-backend/app.js
+++ express-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://kountit.vercel.app/"],
     credentials: true,
   })
 );
