96f694472305f22f2935b9b979fce71873e98ddb
Mohamed <justmoe23@gmail.com>
2023-02-12 18:18:36 -0500

    create user tests

---
+++ express-backend/__tests__/users.test.js
@@ -0,0 +1,34 @@
+const supertest = require("supertest");
+const app = require("../app");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const userId = new mongoose.Types.ObjectId().toString();
+
+require("dotenv").config();
+
+const mockUser = {
+  _id: userId,
+  email: "example@example.com",
+  name: "Example 1",
+};
+
+beforeAll(async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoose.connection.close();
+});
+
+describe("Testing Users Routes", () => {
+  it("should return a 401 status code if a logged out user tried to create a product", async () => {
+    expect(true).toBe(true);
+  });
+});
+
+// What to test
+// 1.) Test that users can register
+// 2.) Test that users can login
+// 3.) Test that users can create new products once they're logged in
