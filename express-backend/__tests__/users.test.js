b95cfd2f418177536766e32cbef436aa6c40fc17
Mohamed <justmoe23@gmail.com>
2023-02-17 22:18:01 -0500

    progress on backend unit testing

--- express-backend/__tests__/users.test.js
+++ express-backend/__tests__/users.test.js
@@ -2,15 +2,8 @@ const supertest = require("supertest");
 const app = require("../app");
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
-const userId = new mongoose.Types.ObjectId().toString();
 
-require("dotenv").config();
-
-const mockUser = {
-  _id: userId,
-  email: "example@example.com",
-  name: "Example 1",
-};
+// require("dotenv").config();
 
 beforeAll(async () => {
   const mongoServer = await MongoMemoryServer.create();
@@ -23,8 +16,48 @@ afterAll(async () => {
 });
 
 describe("Testing Users Routes", () => {
-  it("should return a 401 status code if a user tried to create an account with existing credentials", async () => {
-    expect(true).toBe(true);
+  describe("User Registration", () => {
+    it("User that doesn't exist should be able to create an account", async () => {
+      const fakeUser = await supertest(app).post("/api/users/register").send({
+        name: "Testing",
+        email: "test@test.com",
+        password: "testing12345",
+      });
+
+      expect(fakeUser.statusCode).toBe(200);
+    });
+
+    it("User with missing credentials shouldn't be able to create an account", async () => {
+      const fakeUser = await supertest(app).post("/api/users/register").send({
+        name: "Testing",
+        email: "test@test.com",
+      });
+
+      expect(fakeUser.statusCode).toBe(400);
+    });
+
+    it("User with password length less than 8 characters long should get a 400 status code error", async () => {
+      const fakeUser = await supertest(app).post("/api/users/register").send({
+        name: "Testing",
+        email: "test@test.com",
+        password: "test",
+      });
+
+      expect(fakeUser.statusCode).toBe(400);
+    });
+
+    // Work on this test -> returns status code 200 when it should return 400 since the email already exists. Possible issue: since these calls are being made to a mongodb database in memory, it is possible that after every instance of testing and when the server is closed, then all data is deleted.
+    it("User with an email that is already in use should not be able to create an account", async () => {
+      const fakeUser = await supertest(app).post("/api/users/register").send({
+        name: "Testing",
+        email: "jane@doe.com",
+        password: "testing12345",
+      });
+
+      console.log(fakeUser);
+
+      expect(fakeUser.statusCode).toBe(400);
+    });
   });
 });
 
