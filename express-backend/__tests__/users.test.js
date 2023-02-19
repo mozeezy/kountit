c53980586aa1c0b09c3d2eab848f8752ab709057
Mohamed <justmoe23@gmail.com>
2023-02-18 19:55:24 -0500

    finish testing user-related endpoints

--- express-backend/__tests__/users.test.js
+++ express-backend/__tests__/users.test.js
@@ -5,6 +5,17 @@ const { MongoMemoryServer } = require("mongodb-memory-server");
 
 // require("dotenv").config();
 
+const fakeUser = {
+  name: "Testing",
+  email: "test@test.com",
+  password: "testing12345",
+};
+
+const fakeUserLogin = {
+  email: "test@test.com",
+  password: "testing12345",
+};
+
 beforeAll(async () => {
   const mongoServer = await MongoMemoryServer.create();
   await mongoose.connect(mongoServer.getUri());
@@ -17,51 +28,76 @@ afterAll(async () => {
 
 describe("Testing Users Routes", () => {
   describe("User Registration", () => {
-    it("User that doesn't exist should be able to create an account", async () => {
-      const fakeUser = await supertest(app).post("/api/users/register").send({
-        name: "Testing",
-        email: "test@test.com",
-        password: "testing12345",
-      });
+    it("User that doesn't exist should be able to create an account and get a 200 status code", async () => {
+      const response = await supertest(app)
+        .post("/api/users/register")
+        .send(fakeUser);
 
-      expect(fakeUser.statusCode).toBe(200);
+      expect(response.statusCode).toBe(200);
     });
 
-    it("User with missing credentials shouldn't be able to create an account", async () => {
-      const fakeUser = await supertest(app).post("/api/users/register").send({
+    it("User with missing credentials should get a 400 status code", async () => {
+      const response = await supertest(app).post("/api/users/register").send({
         name: "Testing",
         email: "test@test.com",
       });
 
-      expect(fakeUser.statusCode).toBe(400);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Please fill in the missing fields.");
     });
 
-    it("User with password length less than 8 characters long should get a 400 status code error", async () => {
-      const fakeUser = await supertest(app).post("/api/users/register").send({
+    it("User with password length less than 8 characters long should get a 400 status code", async () => {
+      const response = await supertest(app).post("/api/users/register").send({
         name: "Testing",
         email: "test@test.com",
         password: "test",
       });
 
-      expect(fakeUser.statusCode).toBe(400);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Password must contain 8 characters.");
     });
 
-    // Work on this test -> returns status code 200 when it should return 400 since the email already exists. Possible issue: since these calls are being made to a mongodb database in memory, it is possible that after every instance of testing and when the server is closed, then all data is deleted.
-    it("User with an email that is already in use should not be able to create an account", async () => {
-      const fakeUser = await supertest(app).post("/api/users/register").send({
-        name: "Testing",
-        email: "jane@doe.com",
+    it("User with an email that is already in use should not be able to create an account and should get a 400 status code", async () => {
+      const response = await supertest(app)
+        .post("/api/users/register")
+        .send(fakeUser);
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe(
+        "A user with that email already exists. Please use a different email address."
+      );
+    });
+  });
+
+  describe("User Login", () => {
+    it("User that already exist should be able to login and get a 200 status code.", async () => {
+      const response = await supertest(app)
+        .post("/api/users/login")
+        .send(fakeUserLogin);
+
+      expect(response.statusCode).toBe(200);
+    });
+
+    it("User that does not exist shouldn't be able to login and get a 400 status code.", async () => {
+      const response = await supertest(app).post("/api/users/login").send({
+        email: "test@example.com",
         password: "testing12345",
       });
 
-      console.log(fakeUser);
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe(
+        "This user does not exist. Please sign up."
+      );
+    });
 
-      expect(fakeUser.statusCode).toBe(400);
+    it("User with missing credentials shouldn't be able to login and get a 400 status code.", async () => {
+      const response = await supertest(app).post("/api/users/login").send({
+        email: "test@test.com",
+        password: "",
+      });
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("Please fill in the missing fields.");
     });
   });
 });
-
-// What to test
-// 1.) Test that users can register
-// 2.) Test that users can login
-// 3.) Test that users can create new products once they're logged in
