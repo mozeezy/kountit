1cdb1f348e7ef04c3a84d4a9af1a421f040a2f1f
Mohamed <justmoe23@gmail.com>
2023-02-22 21:12:54 -0500

    write product integration tests

--- express-backend/__tests__/integration/products.test.js
+++ express-backend/__tests__/integration/products.test.js
@@ -24,8 +24,13 @@ afterAll(async () => {
 
 describe("Testing Product Routes", () => {
   it("should return a 401 status code if a logged out user tried to create a product", async () => {
-    const { statusCode } = await supertest(app).post("/api/products");
+    const { statusCode, body } = await supertest(app)
+      .post("/api/products")
+      .send({});
     expect(statusCode).toBe(401);
+    expect(body.message).toBe(
+      "You're not authorized to access this page. Please login to view the page."
+    );
   });
 
   it("should return a 200 status code if the user is logged and tried to create a product", async () => {
